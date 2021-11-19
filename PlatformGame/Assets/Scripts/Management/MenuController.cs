6335fdaa05c8bfc4501d54b8d1c56ed894876dc4
Victor <81108091+vgArchives@users.noreply.github.com>
2021-11-19 12:02:14 -0300

    Update MenuController.cs

--- PlatformGame/Assets/Scripts/Management/MenuController.cs
+++ PlatformGame/Assets/Scripts/Management/MenuController.cs
@@ -7,18 +7,6 @@ public class MenuController : MonoBehaviour
     [SerializeField] private Canvas menuCanvas;
     [SerializeField] private Canvas howToPlayCanvas;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void HowToPlay()
     {
         menuCanvas.gameObject.SetActive(false);
